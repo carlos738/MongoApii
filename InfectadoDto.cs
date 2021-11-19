ad1dccd6a8ed349d76781edf315147fbc2cf1837
carlos738 <carlosasl135@hotmail.com>
2021-11-18 21:51:43 -0300

    Add files via upload

---
+++ InfectadoDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System;
+
+namespace Api.Models
+{
+    public class InfectadoDto
+    {
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
